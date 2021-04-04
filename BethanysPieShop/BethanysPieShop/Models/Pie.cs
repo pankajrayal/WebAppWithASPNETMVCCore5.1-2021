ead7230604af9380da5c9f68dc6cc346ed27a8cd
Pankaj Rayal <https://github.dxc.com/>
2021-04-04 20:05:42 +0530

    05-Working with Real Data Using Entity Framework Core

--- BethanysPieShop/BethanysPieShop/Models/Pie.cs
+++ BethanysPieShop/BethanysPieShop/Models/Pie.cs
@@ -12,5 +12,6 @@
         public bool InStock { get; set; }
         public int CategoryId { get; set; }
         public Category Category { get; set; }
+        public string Notes { get; set; }
     }
 }
